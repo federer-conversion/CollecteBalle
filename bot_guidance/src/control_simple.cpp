c93f2a0ee6f6b482bc398c73d75719241d897f1c
defouni <nicolas-defour@ensta-bretagne.org>
2023-01-13 12:01:55 +0100

    WIP: test control_simple

--- bot_guidance/src/control_simple.cpp
+++ bot_guidance/src/control_simple.cpp
@@ -8,6 +8,9 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <std_msgs/msg/bool.hpp>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/utils.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 using namespace std;
 
@@ -24,22 +27,23 @@ class Subscriber_publisher : public rclcpp::Node
       commande_pub = this->create_publisher<geometry_msgs::msg::Twist>("/commande_robot", 1000);
       timer_ = this->create_wall_timer(500ms, std::bind(&Subscriber_publisher::timer_callback, this));
       subscriptionBotPose_ = this->create_subscription<geometry_msgs::msg::Pose>("/position_robot", 1000, bind(&Subscriber_publisher::PositionCallBack, this, placeholders::_1));
-      subscriptionGoalPose_ = this->create_subscription<geometry_msgs::msg::Pose>("/position_balle", 1000, bind(&Subscriber_publisher::PositionCibleCallBack, this, placeholders::_1));
+      subscriptionGoalPose_ = this->create_subscription<geometry_msgs::msg::Pose>("/position_r_balle", 1000, bind(&Subscriber_publisher::PositionCibleCallBack, this, placeholders::_1));
     }
 
   private:
 
     void PositionCallBack(const geometry_msgs::msg::Pose::SharedPtr message){
     // position du robot dans le repere du terrain
-    // lorsqu'une nouvelle position/orientation est disponible, recuperation de cette information
     X[0] = message->position.x;
     X[1] = message->position.y;
     X[2] = 0.;
-    // yaw = tf::getYaw(message->orientation);
+    yaw = tf2::getYaw(message->orientation);
+    cout << "X = (" << X[0] << ", " << X[1] << ", " << X[2] << endl;
+    cout << "message->position.x" << message->position.x << endl;
     }
 
     void PositionCibleCallBack(const geometry_msgs::msg::Pose::SharedPtr message){
-    //nouvelle position cible
+    //position de la balle de tennis
     Y[0] = message->position.x;
     Y[1] = message->position.y;
     Y[2] = 0.;
@@ -58,10 +62,17 @@ class Subscriber_publisher : public rclcpp::Node
         
         delta_theta = theta_voulu-yaw;
         e = 2*atan(tan(delta_theta/2));
-        msg.angular.z = -k*e;
+        msg.angular.z = k*e;
 
         //publication du message
-        msg.linear.x = -8;
+        double dx,dy;
+        double k_linear = 1;
+        dx = X[0] - Y[0];
+        dy = X[1] - Y[1];
+        cout << "dx" << dx << endl;
+        double distance = sqrt( dx*dx + dy*dy);
+        cout << distance << endl;
+        msg.linear.x = k_linear*distance;
         commande_pub->publish(msg);
         }
 
