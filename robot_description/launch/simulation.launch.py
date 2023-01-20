6df4febd4eb039faa9d049cc71229fca31bb7557
mirado <mirado.rajaomarosata@ensta-bretagne.org>
2023-01-20 16:58:45 +0100

    Done: Control pince, pb collision et inertie

--- robot_description/launch/simulation.launch.py
+++ robot_description/launch/simulation.launch.py
@@ -4,6 +4,8 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir
 
+from launch.actions import ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
 
@@ -18,4 +20,15 @@ def generate_launch_description():
     return launch.LaunchDescription([
         display_launch,
         tennis_court_launch,
+        
+        
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=display_launch,
+        #         on_exit=[tennis_court_launch],
+        #     )
+        # ),
+
+        # display_launch
+
     ])
